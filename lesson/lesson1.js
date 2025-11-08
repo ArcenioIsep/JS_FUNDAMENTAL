3b9c122120c830c39a3e9553e2e0347eabeeeb7b
unknown <1171351@isep.ipp.pt>
2025-11-07 18:02:50 -0800

    vareble ,constant and data type

--- lesson/lesson1.js
+++ lesson/lesson1.js
@@ -1,2 +1,22 @@
 // 1 heloo world
 console.log("hello Wortd!")
+// variable
+var firstName="jose"
+let lastName="Machava"
+console.log(firstName)
+console.log(lastName) 
+/// we can declare the vareble fiste ans after use  like that 
+var  age,dateOfB,sex
+age="5"
+sex="Male"
+console.log(age) 
+/// constant  ---  tens que inicilizar a constante aste de  usar e ela nao pode  ser alterada 
+const occupation="pedreiro"// coantante inicializada 
+console.log(occupation)
+
+// date type
+var mindleName=""
+var age=25
+var isheMarriged=true
+var numberOfCart=undefined;//we eill se in erro state we can not use something that doest exit
+
