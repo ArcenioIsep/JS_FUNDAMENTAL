6ac028a9b4c0400e8b0281023edeafa73af4da21
unknown <1171351@isep.ipp.pt>
2025-11-28 14:47:19 -1200

    array in  and object  usning js

---
+++ lesson/lesson3.js
@@ -0,0 +1,19 @@
+//var firstName="jose"
+//let lastName="Machava"
+
+//object
+custumer={
+    firstName :'jose ',
+     lastName :'pereira ',
+     // the custumer has car 
+  car:["Toyota ","MAzda ","volvo"]
+
+
+};
+// here we get acesss of the information on the object 
+console.log(custumer.lastName+ custumer.firstName);
+
+/// the custumer  name has this car 
+
+// Array
+console.log("Name "+custumer.firstName, "Drive "+custumer.car[0]); // tha the idex of the array
