d7b899d40c22d0375abfa8de7c09488833261c4c
unknown <1171351@isep.ipp.pt>
2025-12-06 14:48:19 -1200

    comperasing operator

---
+++ lesson/lesson4.js
@@ -0,0 +1,14 @@
+//  Realational or comparaomparison oparator 
+// > - more than
+// < -less than
+// >= -more than equal
+// <= - less than equal
+// is 10 more tham 5 
+var  result =10>=5
+// this gonna return a boolean 
+console.log(result) 
+// equal operation
+var  x =1;
+console.log(x=='1');// lose comparesion
+//  with 3 === que are not justa compre the value but we are compare the data typ of value 
+console.log(x==='1');// Strict  compararion   this will 
